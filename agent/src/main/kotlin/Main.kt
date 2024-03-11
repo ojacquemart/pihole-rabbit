57c9d00237127e92f22db65298d2eb1d61ba48f9
ojacquemart <o.jacquemart@gmail.com>
2024-03-11 22:43:02 +0100

    docs: add wireframes and think about building crontabs

--- agent/src/main/kotlin/Main.kt
+++ agent/src/main/kotlin/Main.kt
@@ -5,6 +5,9 @@ import com.githuh.pihole.rabbit.com.github.ojacquemart.pihle.rabbit.agent.PiHole
 
 suspend fun main() {
     // TODO: try to generate a crontab and see if it could be executed
+    // >> it needs crontab -l > /tmp/crontab.txt
+    // >> create a file with the content of the new elements
+    // >> crontab /tmp/crontab.txt to update the crontab
     // TODO: read a config file
     // TODO: think about a way to execute the main function forever (while true?)
     // TODO: try to generate a jar and test it on the Raspberry Pi
@@ -15,5 +18,8 @@ suspend fun main() {
             password = null,
         )
     )
+
     println(foo.getGroups())
+    println(foo.getClients())
+    println(foo.getDomains())
 }
